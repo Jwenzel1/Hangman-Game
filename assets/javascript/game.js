7493930a079ef980190ec1803cc5d1c158d47974
gatan45 <joewenzel45@gmail.com>
2017-07-25 11:35:08 -0400

    The user can put in inputs and they will reveal in the play area if they are correct. still need to implement the win/loss logic and outputting the incorrect guesses the user made.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,65 @@
-var dogs = ["Affenpinscher", "AfghanHound", "AiredaleTerrier", "Akita"];
+var dogs = ["Akita"];
+var word;
+var guessedLetters = [];
 var gameStarted = false;
 var guessesRemaining = 10;
+var winsCounter = 0;
 
 //Event Listener that handles the key presses
 document.onkeyup = function(event){
   if(gameStarted){
-    var buttonPressed = event.key;
-    if(
-      (buttonPressed.charCodeAt() >= 'A'.charCodeAt() && buttonPressed.charCodeAt() <= 'Z'.charCodeAt()) ||
-      (buttonPressed.charCodeAt() >= 'a'.charCodeAt() && buttonPressed.charCodeAt() <= 'z'.charCodeAt())){
-        console.log(buttonPressed);
+    var buttonPressed = event.key.toLowerCase();
+    if((buttonPressed.charCodeAt() >= 'A'.charCodeAt() && buttonPressed.charCodeAt() <= 'Z'.charCodeAt()) ||
+        (buttonPressed.charCodeAt() >= 'a'.charCodeAt() && buttonPressed.charCodeAt() <= 'z'.charCodeAt())){
+        //console.log(buttonPressed);
+        if(buttonPressed !== "Control" && buttonPressed !== "Alt"){
+          if(word.indexOf(buttonPressed) > -1){
+            var playArea = document.getElementById("playArea");
+            for(var i = 0; i < word.length; i++){
+              if(word[i].toLowerCase() === buttonPressed){
+                //var replacement = document.createElement("p").appendChild(document.createTextNode(buttonPressed));
+                var replacement = document.createTextNode(buttonPressed + "  ");
+                playArea.replaceChild(replacement, playArea.childNodes[i+1]);
+              }
+            }
+
+          }
+          /*if (guessedLetters.indexOf(buttonPressed) === -1) {
+            guessedLetters.push(buttonPressed);
+            guessesRemaining--;
+            updateGuesses();
+          }*/
+
+        }
     }
   }
   else{
     var dog = dogs[Math.floor(Math.random() * dogs.length)];
     var playArea = document.getElementById("playArea");
     for (var i = 0; i < dog.length; i++) {
-      var space = document.createElement("p").appendChild(document.createTextNode("_  "));
+      var space = document.createElement("P").appendChild(document.createTextNode("_  "));
       playArea.appendChild(space);
     }
-    console.log(document.getElementById('guessesLeft').innerHTML);
-    document.getElementById('guessesLeft').innerHTML.replace("/\\d/", guessesRemaining);
+    word = dog.toLowerCase();
+    updateGuesses();
+    updateWins();
     gameStarted = true;
   }
 
+}
+
+function updateGuesses(){
+  var guesses = document.getElementById('guessesLeft')
+  var newGuesses = document.createElement("p").appendChild(document.createTextNode(guessesRemaining));
+  guesses.removeChild(guesses.childNodes[0]);
+  guesses.appendChild(newGuesses);
+}
+
+function updateWins(){
+  var wins = document.getElementById('winsNum');
+  var newWins = document.createElement("span").appendChild(document.createTextNode(winsCounter));
+  wins.removeChild(wins.childNodes[1]);
+  wins.appendChild(newWins);
 }
 
 //Game Object
