d6af8b2b0beca8577951b1593ae3eac4a2eae6d6
gatan45 <joewenzel45@gmail.com>
2017-07-26 14:09:08 -0400

    Did some more refactoring. Moved the functions to the top to stay in line with standards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,86 @@
 var dogs = ["Affenpinscher", "Akita", "Azawakh", "Barbet", "Basenji", "Beagle", "Bloodhound", "Bolognese", "Borzoi", "Boxer", "Briard", "Brittany", "Bulldog", "Bullmastiff", "Chihuahua", "Chinook", "Cockapoo", "Collie", "Dachshund", "Dalmatian", "Goldador", "Goldendoodle", "Greyhound", "Harrier", "Havanese", "Keeshond", "Komondor", "Kooikerhondje", "Kuvasz", "Labradoodle", "Leonberger", "Lowchen", "Maltese", "Maltipoo", "Mastiff", "Mutt", "Newfoundland", "Otterhound", "Papillon", "Peekapoo", "Pekingese", "Plott", "Pointer", "Pomeranian", "Poodle", "Pug", "Puggle", "Puli", "Rottweiler", "Saluki", "Samoyed", "Schipperke", "Schnoodle", "Sloughi", "Stabyhoun", "Vizsla", "Weimaraner", "Whippet", "Xoloitzcuintli", "Yorkipoo"];
-
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
 var gameStarted = false;
 var winsCounter = 0;
 var currentGame;
+
+function getPlayArea(){
+  return document.getElementById("playArea");
+}
+
+function addToGuessArea(letter){
+  var area = document.getElementById("guessesArea");
+  var child = document.createElement("span");
+  child.textContent = letter;
+  area.appendChild(child);
+}
+
+function updatePlayArea(word, key){
+  var playAreaKids = getPlayArea().childNodes;
+  for(var i = 0; i < word.length; i++){
+    if(word[i] === key){
+      playAreaKids[i].textContent = key;
+    }
+  }
+}
+
+function setUpPlayArea(gameObj){
+  var playArea = getPlayArea();
+  for(var i = 0; i < gameObj.word.length; i++){
+    var space = document.createElement("span");
+    space.textContent = "_";
+    playArea.appendChild(space);
+  }
+}
+
+function purgeChildren(id){
+  var parent = document.getElementById(id);
+  while(parent.childNodes.length != 0){
+    parent.removeChild(parent.childNodes[0]);
+  }
+}
+
+function update(id, value){
+  document.getElementById(id).textContent = value;
+}
+
+function checkForWin(){
+  var playAreaNodes = getPlayArea().childNodes;
+  for(var i = 0; i < playAreaNodes.length; i++){
+    if(playAreaNodes[i].textContent === "_"){
+      return false;
+    }
+  }
+  return true;
+}
+
+//Game Object
+function Game(word){
+  this.word = word;
+  this.numGuesses = 11; //needs to be one more than what you want it to be
+  this.wrongLetters = [];
+  this.correctLetters = [];
+
+  this.updateGuesses = function(){
+    this.numGuesses--;
+    document.getElementById("guessesLeft").textContent = this.numGuesses;
+  }
+
+  this.addGuessedLetter = function(letter){
+    if(this.word.indexOf(letter) > -1){
+      this.correctLetters.push(letter);
+      return true;
+    }
+    else{
+      this.wrongLetters.push(letter);
+      return false;
+    }
+  }
+
+  this.alreadyGuessed = function(letter){
+    return(this.correctLetters.indexOf(letter) > -1 || this.wrongLetters.indexOf(letter) > -1);
+  }
+}
 
 //Event Listener that handles the key presses
 document.onkeyup = function(event){
@@ -42,82 +118,4 @@ document.onkeyup = function(event){
     setUpPlayArea(currentGame);
     gameStarted = true;
   }
-};
-
-function checkForWin(){
-  var playAreaNodes = getPlayArea().childNodes;
-  for(var i = 0; i < playAreaNodes.length; i++){
-    if(playAreaNodes[i].textContent === "_"){
-      return false;
-    }
-  }
-  return true;
-};
-
-function addToGuessArea(letter){
-  var area = document.getElementById("guessesArea");
-  var child = document.createElement("span");
-  child.textContent = letter;
-  area.appendChild(child);
-};
-
-function updatePlayArea(word, key){
-  var playAreaKids = getPlayArea().childNodes;
-  for(var i = 0; i < word.length; i++){
-    if(word[i] === key){
-      playAreaKids[i].textContent = key;
-    }
-  }
-};
-
-function setUpPlayArea(gameObj){
-  var playArea = getPlayArea();
-  for(var i = 0; i < gameObj.word.length; i++){
-    var space = document.createElement("span");
-    space.textContent = "_";
-    playArea.appendChild(space);
-  }
-};
-
-function purgeChildren(id){
-  var parent = document.getElementById(id);
-  while(parent.childNodes.length != 0){
-    parent.removeChild(parent.childNodes[0]);
-  }
-};
-
-function update(id, value){
-  document.getElementById(id).textContent = value;
-};
-
-function getPlayArea(){
-  return document.getElementById("playArea");
-};
-
-//Game Object
-function Game(word){
-  this.word = word;
-  this.numGuesses = 11; //needs to be one more than what you want it to be
-  this.wrongLetters = [];
-  this.correctLetters = [];
-
-  this.updateGuesses = function(){
-    this.numGuesses--;
-    document.getElementById("guessesLeft").textContent = this.numGuesses;
-  };
-
-  this.addGuessedLetter = function(letter){
-    if(this.word.indexOf(letter) > -1){
-      this.correctLetters.push(letter);
-      return true;
-    }
-    else{
-      this.wrongLetters.push(letter);
-      return false;
-    }
-  };
-
-  this.alreadyGuessed = function(letter){
-    return(this.correctLetters.indexOf(letter) > -1 || this.wrongLetters.indexOf(letter) > -1);
-  };
-};
+}
